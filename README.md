453266f00e2e6c8ba1b8b6ddba6f02a9cb0ec27b
Beka Chaduneli <120742353+bekaChaduneli@users.noreply.github.com>
2023-02-01 01:26:21 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+
+## Overview
+
+### Screenshot
+![screenshot](/design/desktop-preview.jpg)
+
+
+### Links
+
+- Solution URL: [GitHub](https://bekaChaduneli.github.io/launch-countdown-timer)
+- Live Site URL: [Live URL](https://github.com/bekaChaduneli/launch-countdown-timer)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Mobile-first workflow
+- JavaScript
+- Flexbox
+
+### What I learned
+
+Learned how to use setInterval better
+
+## Author
+
+- LinkedIn- [Beka Chaduneli](https://www.linkedin.com/in/beka-chaduneli-28203422b/)
---
+++ script.js
@@ -0,0 +1,49 @@
+"use strict";
+const daysBackground = document.querySelector(".days-timer-background");
+const hoursBackground = document.querySelector(".hours-timer-background");
+const minutesBackground = document.querySelector(".minutes-timer-background");
+const secondsBackground = document.querySelector(".seconds-timer-background");
+
+const countDown = () => {
+    const endDate = new Date("april, 31, 2023 20:17:00").getTime()
+    const now = new Date().getTime()
+    
+    const difference  = endDate - now
+
+    const seconds = 1000
+    const minutes = seconds * 60
+    const hours = minutes * 60
+    const days = hours *24
+    const timeDays = Math.floor(difference / days)
+    const timeHours = Math.floor((difference & days) / hours)
+    const timeMinutes = Math.floor((difference % hours) / minutes)
+    const timeSeconds = Math.floor((difference % minutes)/seconds)
+    if(document.querySelector(".seconds-text").innerHTML = timeSeconds < 10) {
+        document.querySelector(".seconds-text").innerHTML = `0${timeSeconds}`;
+    }
+    else {
+        document.querySelector(".seconds-text").innerHTML = timeSeconds;
+    }
+    if(document.querySelector(".hours-text").innerHTML = timeHours < 10) {
+        document.querySelector(".hours-text").innerHTML = `0${timeHours}`;
+    }
+    else {
+        document.querySelector(".hours-text").innerHTML = timeHours;
+    }
+    if (document.querySelector(".minutes-text").innerHTML = timeMinutes <10) {
+        document.querySelector(".minutes-text").innerHTML = `0${timeMinutes}`;
+    }
+    else {
+        document.querySelector(".minutes-text").innerHTML = timeMinutes;
+    }
+    if(document.querySelector(".days-text").innerHTML = timeDays < 10){
+        document.querySelector(".days-text").innerHTML = `0${timeDays}`
+    }
+    else {
+        document.querySelector(".days-text").innerHTML = timeDays;
+    }
+}
+
+setInterval(countDown, 1000);
+
+
